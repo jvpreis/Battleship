aecda91daa85905e38ecafb076d90ce0e3817fe0
José reis <jvprs@iscte-iul.pt>
2026-09-22 16:57:15 +0100

    atualização do javadoc classe task

--- src/main/java/iscteiul/ista/battleship/Tasks.java
+++ src/main/java/iscteiul/ista/battleship/Tasks.java
@@ -84,7 +84,7 @@ public class Tasks {
 
     /**
      * This task tests the building up of fleets and takes into consideration the
-     * possibility of cheating
+     * possibility of cheating.
      */
     public static void taskC() {
         Scanner in = new Scanner(System.in);
