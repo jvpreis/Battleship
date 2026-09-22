dc75140791ec9acd99273102b16bc8c00338d88f
José reis <jvprs@iscte-iul.pt>
2026-09-22 16:53:44 +0100

    atualização do javadoc classe task

--- src/main/java/iscteiul/ista/battleship/Tasks.java
+++ src/main/java/iscteiul/ista/battleship/Tasks.java
@@ -49,7 +49,7 @@ public class Tasks {
     }
 
     /**
-     * This task tests the building up of fleets and the printing of their status
+     * This task tests the building up of fleets and the printing of their status.
      */
     public static void taskB() {
         Scanner in = new Scanner(System.in);
